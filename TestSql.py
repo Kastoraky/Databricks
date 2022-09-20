34412ff2b6a137dabc2e2d790c42eb91d4d786a0
Kastoraky@gmail.com <Kastoraky@gmail.com>
2022-09-20 10:12:04 +0000

    My Test Note
    My First Test

---
+++ TestSql.py
@@ -0,0 +1,46 @@
+# Databricks notebook source
+from pyspark.sql import *
+import pandas as pd
+
+# COMMAND ----------
+
+jdbcHostname = "mypersonaldatabaseserver.database.windows.net"
+jdbcDatabase = "Dictionary"
+jdbcPort = 1433
+jdbcUsername = "Kastoraky"
+jdbcPassword = "B.k.12#$56"
+jdbcUrl = "jdbc:sqlserver://{0}:{1};database={2};user={3};password={4}".format(jdbcHostname, jdbcPort, jdbcDatabase, jdbcUsername, jdbcPassword)
+
+# COMMAND ----------
+
+jdbcUrl = "jdbc:sqlserver://{0}:{1};database={2}".format(jdbcHostname, jdbcPort, jdbcDatabase)
+connectionProperties = {
+  "user" : jdbcUsername,
+  "password" : jdbcPassword,
+  "driver" : "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+}
+
+# COMMAND ----------
+
+empl = "(SELECT TOP (1000) * FROM [dbo].[Employee]) emp_alias"
+df = spark.read.jdbc(url=jdbcUrl, table=empl, properties=connectionProperties)
+df.createOrReplaceTempView("source") 
+
+
+
+# COMMAND ----------
+
+dataS = "(SELECT TOP (1000) * FROM [dbo].[Data]) dataS"
+df_dataS = spark.read.jdbc(url=jdbcUrl, table=dataS, properties=connectionProperties)
+df_dataS.createOrReplaceTempView("dataS") 
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC SELECT * FROM dataS;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM fpa.Test2
